73f5caa4836e0584201fae18545c0f9d61ee3839
B.Redouane <41746234+RedouaneBenarifJones@users.noreply.github.com>
2021-06-26 15:07:33 +0100

    Add files via upload
    last commit (all good)

--- Josephus.java
+++ Josephus.java
@@ -12,6 +12,7 @@ public class Josephus {
 
         System.out.println("donner le nombre de soldats!");
         int soldiersNumber = scanner.nextInt();
+        System.out.println("");
         String soldierFirstName, soldierLastName;
 
         // population de la linkedlist circulaire (head pointe au tail/ tail pointe au head)
@@ -21,34 +22,27 @@ public class Josephus {
             soldierFirstName = scanner.next();
             System.out.println("donner le nom du soldat");
             soldierLastName = scanner.next();
+            System.out.println("");
             soldiersCircle.add(new Soldier(counter + 1, soldierFirstName, soldierLastName));
             counter++;
         }
 
-        // le dernier noeux de la linkedlist va maintenant pointer vers le head de la linkedlist (circular linkedlist)
-//        soldiersCircle.get(soldiersCircle.size() - 1).nextSoldier = soldiersCircle.getFirst();
-
         // reccuperer le pas
         System.out.println("donner le pas!");
         int step = scanner.nextInt();
+        System.out.println("");
 
         // tuons des soldats maintenant hahahahahaha!
         step--;
-        Josh(soldiersCircle, 0, step);
-
-        //log des fullNames des soldats morts
-        soldiersCircle.forEach(soldier -> {
-            if (!soldier.isDead())
-            System.out.println("le voila josephus " + soldier.getFullName());
-        });
+        josephus(soldiersCircle, 0, step);
 
     }
 
-    static void Josh(ArrayList<Soldier> soldiers, int index, int step)
+    static void josephus(ArrayList<Soldier> soldiers, int index, int step)
     {
-        // Base case , when only one soldiers is left
+        // cas ou un seul soldat est restant
         if (soldiers.size() == 1) {
-            System.out.println(soldiers.get(soldiers.size() - 1));
+            System.out.println("Je suis Josephus le survivant ;D et mon nom complet est " + soldiers.get(soldiers.size() - 1).getFullName());
             return;
         }
 
@@ -57,13 +51,12 @@ public class Josephus {
 //        System.out.println("index = " + index);
 
         // remove the first soldiers which is going to be killed
-        System.out.println("Soldier " + soldiers.get(index).getFullName());
-        System.out.println("Killed at position " + soldiers.get(index).getPosition());
+        System.out.println("Soldat : " + soldiers.get(index).getFullName());
+        System.out.println("tue a la position " + soldiers.get(index).getPosition());
+        System.out.println("");
         soldiers.remove(index);
-        // check soldiers list size
-        System.out.println("soldiers.size = " + soldiers.size());
 
-        // recursive call for n-1 soldierss
-        Josh(soldiers, index, step);
+        // appel recursif por n-1 soldats
+        josephus(soldiers, index, step);
     }
 }
